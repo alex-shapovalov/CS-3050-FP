45f897c9b426834b2c3d60d258cf387ffdc3962a
DremmyII <cbrown54@uvm.edu>
2024-10-14 10:57:12 -0400

    Removed world boundaries

--- creatures.py
+++ creatures.py
@@ -17,21 +17,3 @@ class Creature(arcade.Sprite):
         self.center_x += self.change_x
 
         self.center_y += self.change_y
-
-        # Check for out-of-bounds
-
-        if self.left < 0:
-
-            self.left = 0
-
-        elif self.right > SCREEN_WIDTH - 1:
-
-            self.right = SCREEN_WIDTH - 1
-
-        if self.bottom < 0:
-
-            self.bottom = 0
-
-        elif self.top > SCREEN_HEIGHT - 1:
-
-            self.top = SCREEN_HEIGHT - 1
