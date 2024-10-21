8078717c3eb1065fb71a6e5892dde5579858487d
yonimasha <yonima2003@gmail.com>
2024-10-21 14:47:41 -0400

    added ranged and melee attack classes and fixed simple mistake in player.py

--- player.py
+++ player.py
@@ -13,8 +13,8 @@ class Player(arcade.Sprite):
         )
 
         # following values are subject to change
-        self.health: int = damage
-        self.damage: int = health
+        self.health: int = health
+        self.damage: int = damage
         self.center_x: int = 50
         self.center_y: int = 50
 
@@ -28,3 +28,15 @@ class Player(arcade.Sprite):
         self.center_x += self.change_x
 
         self.center_y += self.change_y
+
+    def receive_damage(self, amount):
+        self.health -= amount
+        if self.health <= 0:
+            self.kill()
+            
+    def give_damage(self, enemy):
+        # if self.attack_type == "melee":
+            # Player is damaged by contact
+            # self.player_sprite.receive_damage(self.damage)
+        # elif self.attack_type == "ranged":
+            # Player is damaged if projectile hits him
---
+++ ranged_attack.py
@@ -0,0 +1,5 @@
+
+class RangedAttack(arcade.Sprite):
+    
+    def __init__(self):
+        pass
